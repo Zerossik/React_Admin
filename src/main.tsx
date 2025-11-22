46163b267f19835b5b6f1d54d7a27ce11529c5d9
Zerossik <zeross19940303@gmail.com>
2025-11-22 12:11:48 +0200

    add base router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
 import "@/styles/index.scss";
 import App from "./App.tsx";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/admin",
+    children: [
+      {
+        index: true,
+        element: <div>Dasboard</div>,
+      },
+      { path: "login", element: <div>Login</div> },
+      { path: "register", element: <div>Register</div> },
+      { path: ":slug/:id?", element: <div>Page with slug</div> },
+    ],
+  },
+
+  { path: "*", element: <div>404 Not found</div> },
+]);
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
